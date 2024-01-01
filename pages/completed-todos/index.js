82394fbe058e4ac33b4606ec33014df8de481a61
Trishala Ghetiya <trishalaghetiya@gmail.com>
2024-01-02 00:50:07 +0530

    code optimization

--- pages/completed-todos/index.js
+++ pages/completed-todos/index.js
@@ -1,40 +1,44 @@
 import { MongoClient } from "mongodb";
 import TodoList from "../../components/todos/TodoList";
+import Head from "next/head";
 
 const completedTodosPage = (props) => {
-    
-    return (
-        <>
-            <TodoList todos={props.todos} />
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Completed Todos</title>
+        <meta name="description" content="List of tasks you have completed"></meta>
+      </Head>
+      <TodoList todos={props.todos} />
+    </>
+  );
+};
 
 export const getStaticProps = async () => {
-    const client = await MongoClient.connect(
-      "mongodb+srv://trishalaghetiya:Th1OXj3NFcM1kTug@cluster0.fer7r1e.mongodb.net/todos?retryWrites=true&w=majority"
-    );
-  
-    const db = client.db();
-  
-    const todosCollection = db.collection("todos");
-  
-    const todos = await todosCollection.find().toArray();
-  
-    const completedTodos = todos.filter(todo => todo.isDone === true);
-  
-    client.close();
-  
-    return {
-      props: {
-        todos: completedTodos.map((todo) => ({
-          title: todo.title,
-          isDone: todo.isDone,
-          id: todo._id.toString(),
-        })),
-      },
-      revalidate: 10,
-    };
+  const client = await MongoClient.connect(
+    "mongodb+srv://trishalaghetiya:Th1OXj3NFcM1kTug@cluster0.fer7r1e.mongodb.net/todos?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const todosCollection = db.collection("todos");
+
+  const todos = await todosCollection.find().toArray();
+
+  const completedTodos = todos.filter((todo) => todo.isDone === true);
+
+  client.close();
+
+  return {
+    props: {
+      todos: completedTodos.map((todo) => ({
+        title: todo.title,
+        isDone: todo.isDone,
+        id: todo._id.toString(),
+      })),
+    },
+    revalidate: 10,
   };
+};
 
 export default completedTodosPage;
